2713ff8ded49cdc0ff1f1f9c102bea0392eb9ba9
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-05-23 15:04:55 +0200

    Fix navbar

--- record.php
+++ record.php
@@ -17,16 +17,26 @@
         <div class="container-fluid ">
           <a class="anchor" name="top" style="top: 0px;"></a>
           <a class="navbar-brand" href="index.html"><img src="images-video/logodef.png" class="logo" ></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-              <ul class="navbar-nav">
-                <li class="nav-item link"> <a class="nav-link " href="play.html">PLAY</a> </li>
-                <li class="nav-item link"> <a class="nav-link " href="record.PHP">RECORD</a> </li>
-                <li class="nav-item link"> <a class="nav-link" href="products.html">PRODUCTS</a></li>
-                <li class="nav-item link"> <a class="nav-link " href="news.html">NEWS</a> </li>
-                <li class="nav-item link"> <a class="nav-link " href="about.html">ABOUT</a> </li>
-              </ul>
-           </div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="nmainNav" aria-expanded="false" aria-label="Toggle navigation"> 
+            <span class="navbar-toggler-icon"></span> 
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="mainNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item link"><a class="nav-link " href="record.PHP">RECORD</a> </li>
+              <li class="nav-item link">
+                <a class="nav-link" href="products.html">PRODUCTS</a>
+              </li>
+              <li class="nav-item link">
+                <a class="nav-link " href="news.html">NEWS</a>
+              </li>
+              <li class="nav-item link">
+                <a class="nav-link " href="about.html">ABOUT</a>
+              </li>
+              <li class="nav-item link">
+                <a class="nav-link " href="play.html">PLAY</a>
+              </li>
+            </ul>
+         </div>
         </div>
       </nav>
 
